b2d980fc54e7f2b0852e831957887eed4941597a
RistekFasilkom <alifalferdiansyah7@gmail.com>
2024-12-25 19:55:03 +0700

    gdg

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,142 +1,217 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import datetime as dt
 import streamlit as st
+
+# Set the style for seaborn
 sns.set_style("darkgrid")
-st.title('Dashboard Data Pengguna Sepeda Sewa')
-df = pd.read_csv('dashboard/all_data.csv')
-min_date = pd.to_datetime(df["dteday"]).min()
-max_date = pd.to_datetime(df["dteday"]).max()
-with st.sidebar:
-    
-    start_date, end_date = st.date_input(
-        label='Rentang Waktu',min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
-    )
- 
-main_df = df[(df["dteday"] >= str(start_date)) & 
-                (df["dteday"] <= str(end_date))]
 
-st.subheader("Jumlah Penggunaan Sepeda")
-col1, col2, col3 = st.columns(3)
-pie_data = main_df[['casual','registered']].sum()
-total_usage = pie_data.sum()
-with col1:
-    st.metric(f"Total Pengguna Casual ", pie_data['casual'])
-with col2:
-    st.metric(f"Total Pengguna Registered ", pie_data['registered'])
-with col3:
-    st.metric(f"Total Pegguna ", total_usage)
+# Title for the dashboard
+st.title('Dashboard Analisis Faktor Performa Siswa')
 
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.pie(x=pie_data, labels=pie_data.index, autopct='%1.1f%%', startangle=90, wedgeprops={'width': 0.3})
-ax.axis('equal')
+# Load the dataset
+data = pd.read_csv("StudentPerformanceFactors.csv")
+
+# Cleaning Data (Removes missing values)
+data.dropna(inplace=True)
+
+# Show the summary statistics of the data
+st.subheader("Summary Data")
+st.write(data.describe())
+
+
+# Visualisasi: Distribusi Nilai Ujian
+st.subheader('Distribusi Nilai Ujian')
+fig, ax = plt.subplots(figsize=(14, 6))
+sns.histplot(data['Exam_Score'], bins=20, kde=True, ax=ax, color='blue')
+ax.set_title('Distribution of Exam Scores')
+ax.set_xlabel('Exam Score')
+ax.set_ylabel('Frequency')
 st.pyplot(fig)
-st.subheader("Rata-rata Pengguna berdasarkan faktor")
-tab1, tab2, tab3 = st.tabs(["Cuaca", "Musim","Bulan"])
+average_score = data['Exam_Score'].mean()
+st.subheader(f'Rata-rata Nilai Ujian: {average_score:.2f}')
+
+# Count how many scores are above the average
+above_average_count = (data['Exam_Score'] > average_score).sum()
+below_average_count = (data['Exam_Score'] <= average_score).sum()
+
+# Plot the counts
+fig, ax = plt.subplots(figsize=(8, 4))
+sns.barplot(x=['Diatas rata rata', 'Dibawah rata rata'], y=[above_average_count, below_average_count], palette='pastel', ax=ax)
+plt.title('Jumlah Siswa Berdasarkan Nilai Rata-rata')
+plt.ylabel('Jumlah Siswa')
+plt.xlabel('Kategori')
+st.pyplot(fig)
+
+st.subheader('Jumlah data pada dataset  ')
+tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs([
+    "Keterlibatan Orang Tua", "Akses ke Sumber Daya", "Pendapatan Keluarga",
+    "Kualitas Guru", "Jenis Sekolah", "Gangguan Pembelajaran",
+    "Akses Internet", "Gender"
+])
 
 with tab1:
-    st.subheader("Rata-rata Pengguna Kasual dan Terdaftar berdasarkan Cuaca")
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sorted_df = main_df.sort_values(by='cnt', ascending=False)
-    sns.barplot(x=sorted_df['cnt'], y=sorted_df['weathersit'], color='#BA3B46')
-    ax.set_ylabel('Kondisi Cuaca', fontsize=14)
-    ax.set_xlabel('Total rental sepeda', fontsize=14)
-    st.pyplot(fig)
+    st.subheader('Jumlah Berdasarkan Keterlibatan Orang Tua')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='Parental_Involvement', palette='pastel')
+    plt.title('Counts of Parental Involvement')
+    plt.xlabel('Parental Involvement')
+    plt.ylabel('Count')
+    st.pyplot(plt)
 
 with tab2:
-    st.subheader('Rata-Rata Pengguna per hari berdasarkan Musim')
-    data_musim = main_df.groupby('season')[['cnt']].mean().round(1).sort_values('cnt', ascending=False)
-    fig, ax = plt.subplots(figsize = (8,6))
-    sns.barplot(y = data_musim.index, x = data_musim['cnt'], color='#BA3B46')
-    ax.set_ylabel('Musim', fontsize=14)
-    ax.set_xlabel('Total rental sepeda', fontsize=14)
-    ax.set_yticks(data_musim.index)
-    st.pyplot(fig)
+    st.subheader('Jumlah Berdasarkan Akses ke Sumber Daya')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='Access_to_Resources', palette='pastel')
+    plt.title('Counts of Access to Resources')
+    plt.xlabel('Access to Resources')
+    plt.ylabel('Count')
+    st.pyplot(plt)
+
 with tab3:
-    st.title('Rata-Rata Pengguna per hari berdasarkan Bulan')
-    data_perbulan = df.groupby('mnth')[['casual','registered','cnt']].mean().round(1)
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(x=data_perbulan.index, y=data_perbulan['cnt'], hue=data_perbulan['cnt'], palette="Greens_d", dodge=False)
-    ax.set_xlabel('Bulan')
-    ax.set_ylabel('Total rental sepeda', fontsize=14)
-    ax.set_xticks(range(len(data_perbulan.index)))  
-    ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-    plt.tight_layout()
-    st.pyplot(fig) 
+    st.subheader('Jumlah Berdasarkan Pendapatan Keluarga')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='Family_Income', palette='pastel')
+    plt.title('Counts of Family Income')
+    plt.xlabel('Family Income')
+    plt.ylabel('Count')
+    st.pyplot(plt)
 
-st.subheader("Perbandingan pengguna casual dan registered berdasarkan faktor")
-tab1, tab2, tab3, tab4 = st.tabs(["Cuaca", "Hari Libur","Hari Kerja", "Bulan"])
-with tab1:
-    st.subheader('Rata-rata Pengguna Casual dan Registered berdasarkan Cuaca')
-    data_cuaca = main_df.groupby('weathersit')[['casual','registered','cnt']].mean().round(1)
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.bar(data_cuaca.index, data_cuaca['casual'], label='Pengguna Kasual')
-    ax.bar(data_cuaca.index, data_cuaca['registered'], bottom=data_cuaca['casual'], label='Pengguna Terdaftar', color = ['#20BF55'])
-    ax.set_title('Rata-rata Pengguna Kasual dan Terdaftar berdasarkan Cuaca', fontsize=16)
-    ax.set_xlabel('Kondisi Cuaca')
-    ax.set_ylabel('Rata-rata Jumlah Pengguna', fontsize=14)
-    ax.set_xticks(data_cuaca.index)
-    ax.legend()
-    plt.tight_layout()
-    st.pyplot(fig)
-with tab2:
-    st.subheader('Rata-rata Pengguna Kasual dan Terdaftar berdasarkan Hari Libur')
-    grouped_data = main_df.groupby('holiday')[['casual', 'registered']].mean().round(1)
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.bar(grouped_data.index, grouped_data['casual'], label='Pengguna Kasual')
-    ax.bar(grouped_data.index, grouped_data['registered'], bottom=grouped_data['casual'], label='Pengguna Terdaftar', color = ['#20BF55'])
-    ax.set_ylabel('Rata-rata Jumlah Pengguna', fontsize=14)
-
-    ax.set_xticks(grouped_data.index)
-    ax.set_xticklabels(['Bukan Hari Libur', 'Hari Libur'])
-
-    ax.legend()
-    plt.tight_layout()
-    st.pyplot(fig)
-with tab3:
-    grouped_data = main_df.groupby('workingday')[['casual', 'registered']].mean().round(1)
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.bar(grouped_data.index, grouped_data['casual'], label='Pengguna Kasual')
-    ax.bar(grouped_data.index, grouped_data['registered'], bottom=grouped_data['casual'], label='Pengguna Terdaftar', color = ['#20BF55'])
-    st.title('Rata-rata Pengguna Kasual dan Terdaftar berdasarkan Hari Kerja')
-    ax.set_ylabel('Rata-rata Jumlah Pengguna', fontsize=14)
-    ax.set_xticks(grouped_data.index)
-    ax.set_xticklabels(['Bukan Hari Kerja', 'Hari Kerja'])
-    ax.legend()
-    plt.tight_layout()
-    st.pyplot(fig)
 with tab4:
-    st.title('Rata-Rata Pengguna Kasual vs Terdaftar Berdasarkan Bulan')
-    data_perbulan = df.groupby('mnth')[['casual','registered','cnt']].mean().round(1).sort_values('cnt',  ascending=False)
-    fig , ax = plt.subplots(figsize=(10,6))
-    ax.bar(data_perbulan.index,data_perbulan['casual'], label = 'Pengguna Kasual')
-    ax.bar(data_perbulan.index, data_perbulan['registered'], label = 'Pengguna Terdaftar', bottom=data_perbulan['casual'])
-    plt.legend()
-    plt.tight_layout()
-    ax.set_xlabel = "Bulan"
-    ax.set_ylabel('Rata-rata Jumlah Pengguna', fontsize=14)
-    ax.set_xticks(data_perbulan.index)
-    ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-    st.pyplot(fig)
+    st.subheader('Jumlah Berdasarkan Kualitas Guru')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='Teacher_Quality', palette='pastel')
+    plt.title('Counts of Teacher Quality')
+    plt.xlabel('Teacher Quality')
+    plt.ylabel('Count')
+    st.pyplot(plt)
 
-st.subheader('Segmentasi Pengguna Sepeda Berdasarkan Kondisi Operasional')
-col1, col2 = st.columns(2)
-cluster_counts = df['Cluster'].value_counts()
-col_iter = iter([col1, col2])
-for cluster, count in cluster_counts.items():
-    col = next(col_iter)
-    with col:
-        st.metric(label=f"Jumlah Cluster {cluster}", value=count)
-    if col is col2:
-        col_iter = iter([col1, col2])
-fig, ax = plt.subplots()
-def custom_autopct(pct):
-    return ('%.1f%%' % pct) if pct > 5 else ''
-wedges, texts, autotexts = ax.pie(cluster_counts, autopct=custom_autopct , startangle=90)
-ax.axis('equal')  
-ax.legend(wedges, cluster_counts.index, title="Cluster", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+with tab5:
+    st.subheader('Jumlah Berdasarkan Jenis Sekolah')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='School_Type', palette='pastel')
+    plt.title('Counts of School Type')
+    plt.xlabel('School Type')
+    plt.ylabel('Count')
+    st.pyplot(plt)
 
+with tab6:
+    st.subheader('Jumlah Berdasarkan Gangguan Pembelajaran')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='Learning_Disabilities', palette='pastel')
+    plt.title('Counts of Learning Disabilities')
+    plt.xlabel('Learning Disabilities')
+    plt.ylabel('Count')
+    st.pyplot(plt)
+
+with tab7:
+    st.subheader('Jumlah Berdasarkan Akses Internet')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='Internet_Access', palette='pastel')
+    plt.title('Counts of Internet Access')
+    plt.xlabel('Internet Access')
+    plt.ylabel('Count')
+    st.pyplot(plt)
+
+with tab8:
+    st.subheader('Jumlah Berdasarkan Gender')
+    plt.figure(figsize=(8, 4))
+    sns.countplot(data=data, x='Gender', palette='pastel')
+    plt.title('Counts of Gender')
+    plt.xlabel('Gender')
+    plt.ylabel('Count')
+    st.pyplot(plt)
+
+# Visualisasi: Keterlibatan Orang Tua dan Akses ke Sumber Daya
+st.subheader('Rata-rata Nilai Ujian Berdasarkan Keterlibatan Orang Tua dan Akses ke Sumber Daya')
+group1 = data.groupby(['Parental_Involvement', 'Access_to_Resources'])['Exam_Score'].mean().reset_index().sort_values(by='Exam_Score', ascending=True)
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.barplot(data=group1, x='Exam_Score', y='Parental_Involvement', palette='pastel', ax=ax)
+plt.title('Average Exam Scores by Parental Involvement and Access to Resources')
+st.pyplot(fig)
+
+# Visualisasi: Pengaruh Teman Sebaya dan Tingkat Motivasi
+st.subheader('Distribusi Tingkat Motivasi Berdasarkan Pengaruh Teman Sebaya')
+plt.figure(figsize=(10, 6))
+sns.countplot(data=data, x='Peer_Influence', hue='Motivation_Level', palette='pastel')
+plt.title('Distribution of Motivation Levels by Peer Influence')
+plt.xlabel('Peer Influence')
+plt.ylabel('Count')
+st.pyplot(plt)
+
+# Visualisasi: Gender dan Nilai Ujian
+st.subheader('Rata-rata Nilai Ujian Berdasarkan Gender')
+gender_grouped = data.groupby('Gender')['Exam_Score'].mean().reset_index()
+fig, ax = plt.subplots(figsize=(10, 6))
+barplot = sns.barplot(data=gender_grouped, x='Gender', y='Exam_Score', palette='pastel', ax=ax)
+for index, row in gender_grouped.iterrows():
+    barplot.text(index, row['Exam_Score'] + 0.5, round(row['Exam_Score'], 2), color='black', ha="center")
+plt.ylim(60, 70)
+plt.title('Average Exam Scores by Gender')
+plt.xlabel('Gender')
+plt.ylabel('Average Exam Score')
+st.pyplot(fig)
+
+# Visualisasi: Pengaruh Sesi Bimbingan terhadap Nilai Ujian
+st.subheader('Pengaruh Jumlah Sesi Bimbingan terhadap Nilai Ujian')
+tutoring_sessions_grouped = data.groupby('Tutoring_Sessions')['Exam_Score'].mean().reset_index().sort_values(by='Exam_Score', ascending=True)
+fig, ax = plt.subplots(figsize=(8, 6))
+sns.lineplot(data=tutoring_sessions_grouped, x='Tutoring_Sessions', y='Exam_Score', marker='o', ax=ax)
+ax.set_title('Pengaruh Jumlah Sesi Bimbingan terhadap Nilai Ujian')
+ax.set_xlabel('Jumlah Sesi Bimbingan')
+ax.set_ylabel('Rata-rata Nilai Ujian')
+st.pyplot(fig)
+
+# Visualisasi: Pengaruh Jarak Rumah terhadap Nilai Ujian
+st.subheader('Pengaruh Jarak Rumah terhadap Nilai Ujian')
+distance_exam_score = data.groupby(['Distance_from_Home'])['Exam_Score'].mean().reset_index().sort_values(by='Exam_Score', ascending=True)
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.barplot(data=distance_exam_score, x='Distance_from_Home', y='Exam_Score', palette='viridis', ax=ax)
+for index, row in distance_exam_score.iterrows():
+    ax.text(index, row['Exam_Score'] + 0.5, round(row['Exam_Score'], 2), color='black', ha="center")
+plt.ylim(60, 70)
+plt.title('Average Exam Score by Distance from Home')
+plt.xlabel('Distance from Home')
+plt.ylabel('Average Exam Score')
+st.pyplot(fig)
+
+# Visualisasi: Distribusi Nilai Ujian Berdasarkan Tingkat Motivasi
+st.subheader('Distribusi Nilai Ujian Berdasarkan Tingkat Motivasi')
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.boxplot(data=data, x='Motivation_Level', y='Exam_Score', palette='pastel', ax=ax)
+plt.title('Distribution of Exam Scores by Motivation Level')
+plt.xlabel('Motivation Level')
+plt.ylabel('Exam Score')
+plt.xticks(rotation=0)  
+plt.tight_layout()
+st.pyplot(fig)
+
+# Visualisasi: Distribusi Nilai Ujian Berdasarkan Jenis Sekolah dan Kualitas Pengajaran
+
+st.subheader('Distribusi Nilai Ujian Berdasarkan Jenis Sekolah dan Kualitas Pengajaran')
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.boxplot(data=data, x='School_Type', y='Exam_Score', hue='Teacher_Quality', palette='pastel', ax=ax)
+plt.title('Distribution of Exam Scores by School Type and Teacher Quality')
+plt.xlabel('School Type')
+plt.ylabel('Exam Score')
+plt.legend(title='Teacher Quality')
+plt.tight_layout()
+st.pyplot(fig)
+
+
+# Visualisasi: Rata-rata Nilai Ujian Berdasarkan Kehadiran
+
+st.subheader('Rata-rata Nilai Ujian Berdasarkan Kehadiran')
+
+# Group by attendance
+kerajinan = data.groupby('Attendance')['Exam_Score'].mean().reset_index()
+
+# Plot
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.lineplot(data=kerajinan, y='Attendance', x='Exam_Score', marker='o', ax=ax)
+plt.title('Average Exam Score by Attendance')
+plt.ylabel('Attendance')
+plt.xlabel('Average Exam Score')
+plt.ylim(0, 100)
+plt.grid(True)
 st.pyplot(fig)
